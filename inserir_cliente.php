fd95afd4a4b8d9a9d8e72e4b1dd7972fddc74cda
Vinicius Ducatti Ferraz <110203525+vinismo@users.noreply.github.com>
2023-09-18 10:39:21 -0300

    Update inserir_cliente.php

--- inserir_cliente.php
+++ inserir_cliente.php
@@ -1,7 +1,7 @@
 <?php
 
 require(__DIR__ . DIRECTORY_SEPARATOR . 'WebserviceClient.php');
-$host = 'https://HOST/webservice/v1';
+$host = 'https://host/webservice/v1';
 $token = 'token';
 $selfSigned = true;
 $api = new IXCsoft\WebserviceClient($host, $token, $selfSigned);
