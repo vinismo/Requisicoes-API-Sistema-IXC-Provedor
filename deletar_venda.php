1369ba825e1b70ba124d0b3e3bd78161b0d15c14
Vinicius Ducatti Ferraz <110203525+vinismo@users.noreply.github.com>
2023-10-04 16:47:13 -0300

    Add files via upload

--- deletar_venda.php
+++ deletar_venda.php
@@ -1,8 +1,8 @@
 <?php
 
 require(__DIR__ . DIRECTORY_SEPARATOR . 'WebserviceClient.php');
-$host = 'https://192.168.27.108/webservice/v1';
-$token = '11:6a41b053ec3883877c2cc7651e1412e3790be99425492bd144fb944d3d94bb94';
+$host = 'https://host/webservice/v1';
+$token = 'token';
 $selfSigned = true; 
 $api = new IXCsoft\WebserviceClient($host, $token, $selfSigned);
 $registro = '338';
