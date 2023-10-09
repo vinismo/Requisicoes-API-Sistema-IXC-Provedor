fe42ad90298e9e7a48b4c952ddbe00dea9da11cd
Vinicius Ducatti Ferraz <110203525+vinismo@users.noreply.github.com>
2023-10-09 13:20:44 -0300

    Add files via upload

---
+++ inserir_lead.php
@@ -0,0 +1,92 @@
+<?php
+
+
+require(__DIR__ . DIRECTORY_SEPARATOR . 'WebserviceClient.php');
+$host = 'https://host/webservice/v1';
+$token = 'token';
+$selfSigned = true; 
+$api = new IXCsoft\WebserviceClient($host, $token, $selfSigned);
+$dados = array(
+    'principal' => 'N',
+    'id_cliente' => '',
+    'nome' => 'Teste API',
+    'tipo_pessoa' => 'F',
+    'cnpj_cpf' => '',
+    'data_nascimento' => '',
+    'razao' => '',
+    'id_filial' => '',
+    'id_contato_tipo' => '',
+    'id_candidato_tipo' => '',
+    'id_campanha' => '',
+    'id_concorrente' => '',
+    'id_responsavel' => '',
+    'indicado_por' => '',
+    'data_cadastro' => '09/10/2023',
+    'data' => '',
+    'id_vd_contrato' => '',
+    'id_tipo_elemento' => '',
+    'velocidade_calculada' => '',
+    'id_fornecedor' => '',
+    'lead' => 'S',
+    'ativo' => 'S',
+    'id_caixa_ftth' => '',
+    'distancia_caixa_mais_proxima' => '',
+    'id_prospeccao' => '',
+    'ultima_atualizacao' => 'CURRENT_TIMESTAMP',
+    'id_estagio' => '',
+    'ordem_kanban' => '',
+    'fone_residencial' => '',
+    'fone_comercial' => '',
+    'fone_celular' => '999999999999',
+    'fone_whatsapp' => '',
+    'email' => '',
+    'skype' => '',
+    'facebook' => '',
+    'website' => '',
+    'cep' => '',
+    'endereco' => '',
+    'numero' => '',
+    'bairro' => '',
+    'complemento' => '',
+    'cidade' => '',
+    'uf' => '',
+    'referencia' => '',
+    'moradia' => 'P',
+    'tipo_localidade' => 'U',
+    'latitude' => '',
+    'longitude' => '',
+    'external_id' => '',
+    'external_system' => '',
+    'pipe_id_pessoa' => '',
+    'cadastro_site' => 'N',
+    'status_viabilidade' => '',
+    'tipo_rede' => 'P',
+    'operador_neutro' => '',
+    'data_ult_verificacao_viab' => '',
+    'caixa_mais_proxima' => '',
+    'data_cadastro_lead' => '',
+    'velocidade_calculada_lead' => '',
+    'quantidade_pessoas_lead' => '',
+    'quantidade_smart_lead' => '',
+    'frequencia_smart_lead' => '',
+    'quantidade_celular_lead' => '',
+    'frequencia_celular_lead' => '',
+    'quantidade_computador_lead' => '',
+    'frequencia_computador_lead' => '',
+    'quantidade_console_lead' => '',
+    'frequencia_console_lead' => '',
+    'obs' => '',
+    'alerta' => '',
+    'identificador' => '',
+    'origem_medium' => '',
+    'origem_campaing' => '',
+    'origem_source' => '',
+    'identificador_ultima_conversao' => '',
+    'conversao_duplicada_marketing' => ''
+);
+$api->post('contato', $dados);
+$retorno = $api->getRespostaConteudo(false);
+echo '<pre>';
+print_r($retorno);
+
+?>
