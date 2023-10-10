00ac37733dd864f788a22d19b1924d8645b9c364
Vinicius Ducatti Ferraz <110203525+vinismo@users.noreply.github.com>
2023-10-10 16:33:18 -0300

    Add files via upload

---
+++ deletar_prospeccao.php
@@ -0,0 +1,16 @@
+<?php
+
+                    
+require(__DIR__ . DIRECTORY_SEPARATOR . 'WebserviceClient.php');
+$host = 'https://host/webservice/v1';
+$token = 'token';
+$selfSigned = true; 
+$api = new IXCsoft\WebserviceClient($host, $token, $selfSigned);
+$registro = '105';
+$api->delete('crm_canditados', $registro);
+$retorno = $api->getRespostaConteudo(false);
+echo '<pre>';
+print_r($retorno);
+
+?>
+
